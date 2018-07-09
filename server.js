397fd48ebdfc068179328f4a8ffbf49c646f65e7
Brantley Burton <brantleyburton@Brantleys-MacBook-Air.local>
2018-07-09 15:48:35 -0400

    created show page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
-const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/grocery_app_dev';
+const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/musicstore';
 const methodOverride = require('method-override');
 const Album = require('./models/albums.js');
 const albumSeeds = require('./models/seed.js')
@@ -18,6 +18,14 @@ app.post('/albums/', (req, res)=>{
   Album.create(req.body, (error, createdAlbum)=>{
   res.send(createdAlbum);
   });
+});
+
+app.get('/albums/:id', (req, res)=>{
+  Album.findById(req.params.id, (err, foundAlbum)=>{
+        res.render('show.ejs', {
+            album:foundAlbum
+        });
+    });
 });
 
 
